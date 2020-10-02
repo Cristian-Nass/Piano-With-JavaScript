3fd896c7559d5d12d6b8f73d07d6da2de70251c6
Cristian Nasirabadi <cristian.tirdad@gmail.com>
2020-10-02 17:02:29 +0200

    piani is ready

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 
-const keys = document.querySelectorAll('.key');
+const WHITE_KEYS = ['z','x','c','v','b','n','m'];
+const BLACK_KEYS = ['s','d','g','h','j'];
 
-keys.forEach(key => {
-    // key.addEventListener('click', () => playNote(key));
-     key.addEventListener('click', () => playNote());
+const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
+
+keys.forEach((key) => {
+    key.addEventListener('click', () =>  playNote(key));
+  });
+  
+document.addEventListener('keydown', e => {
+    if (e.repeat) return
+    const key = e.key
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+    const blacKeyIndex = BLACK_KEYS.indexOf(key);
+
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+    if (blacKeyIndex > -1) playNote(blackKeys[blacKeyIndex])
 });
 
-function playNote() {
-    console.log('Test');
+function playNote(key) {
+    const noteAudio = document.getElementById(key.dataset.note);
+    noteAudio.currentTime = 0;
+    noteAudio.play();
+    key.classList.add('active');
+    noteAudio.addEventListener('ended', () => {
+        key.classList.remove('active');
+    });
 }
-
-// function playNote(key) {
-//     const noteAudio = document.getElementById(key.dataset.note);
-//     noteAudio.play();
-// }
